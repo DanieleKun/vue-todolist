2b8e55b69a9f3aa64d894b79cffcaec0ad21eabd
Daniele <pucciodaniele95@gmail.com>
2022-05-26 16:04:47 +0200

    method to add todo

---
+++ script.js
@@ -0,0 +1,35 @@
+const root = new Vue({
+    el : "#app",
+    data : {
+        newTodo : "",
+        todos : [
+            {
+                text : "Ricordati di respirare",
+                done: false
+            },
+            {
+                text : "Studiare",
+                done: true
+            },
+            {
+                text : "Andare a mare",
+                done: false
+            }
+        ]
+    },
+    methods : {
+        addItem(){
+            //creo nuovo oggetto
+            const addTodo = new Object();
+            addTodo.text = this.newTodo;
+            addTodo.done = true;
+            //pusho nuovo oggetto e pulisco input
+            if(this.newTodo !== ""){
+                this.todos.push(addTodo);
+                this.newTodo = "";
+            }
+            
+        }
+    }
+
+});
